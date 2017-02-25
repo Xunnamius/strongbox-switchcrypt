62ffe09498ea8334d0f872565c307fa444a8f907
Xunnamius (Anarchy) <Xunnamius@users.noreply.github.com>
2017-02-25 02:12:55 -0600

    let the testing begin!

--- src/bitmask.c
+++ src/bitmask.c
@@ -2,51 +2,78 @@
 
 bitmask_t * bitmask_init(unsigned int length)
 {
-    return NULL;
+    bitmask_t * bitmask = (bitmask_t *) malloc(sizeof(bitmask_t));
+    bitmask->byte_length = length;
+    bitmask->mask = calloc(length, sizeof(char));
+    return bitmask;
 }
 
 void bitmask_fini(bitmask_t * bitmask)
 {
+    free(bitmask->mask);
+    free(bitmask);
 }
 
 void bitmask_set_bit(bitmask_t * bitmask, unsigned int index)
 {
+    (void) bitmask;
+    (void) index;
 }
 
 void bitmask_clear_bit(bitmask_t * bitmask, unsigned int index)
 {
+    (void) bitmask;
+    (void) index;
 }
 
 void bitmask_toggle_bit(bitmask_t * bitmask, unsigned int index)
 {
+    (void) bitmask;
+    (void) index;
 }
 
 void bitmask_set_mask(bitmask_t * bitmask)
 {
+    (void) bitmask;
 }
 
 void bitmask_clear_mask(bitmask_t * bitmask)
 {
+    (void) bitmask;
 }
 
 void bitmask_set_bits(bitmask_t * bitmask, unsigned int start_index, unsigned int length)
 {
+    (void) bitmask;
+    (void) start_index;
+    (void) length;
 }
 
 void bitmask_clear_bits(bitmask_t * bitmask, unsigned int start_index, unsigned int length)
 {
+    (void) bitmask;
+    (void) start_index;
+    (void) length;
 }
 
 void bitmask_toggle_bits(bitmask_t * bitmask, unsigned int start_index, unsigned int length)
 {
+    (void) bitmask;
+    (void) start_index;
+    (void) length;
 }
 
 int bitmask_is_bit_set(bitmask_t * bitmask, unsigned int index)
 {
+    (void) bitmask;
+    (void) index;
     return 0;
 }
 
 int bitmask_are_bits_set(bitmask_t * bitmask, unsigned int start_index, unsigned int length)
 {
+    (void) bitmask;
+    (void) start_index;
+    (void) length;
     return 0;
 }
--- test/test_bitmask.c
+++ test/test_bitmask.c
@@ -1,17 +1,130 @@
 #include "unity.h"
 #include "bitmask.h"
 
-void setUp()
-{
+#define TRY_FN_CATCH_EXCEPTION(fn_call)           \
+e_actual = EXCEPTION_NO_EXCEPTION;                \
+Try                                               \
+{                                                 \
+    fn_call;                                      \
+    TEST_FAIL();                                  \
+}                                                 \
+Catch(e_actual)                                   \
+    TEST_ASSERT_EQUAL_INT(e_expected, e_actual);
 
+#define BITMASK_BYTE_LENGTH 4 // 32 bits
+static bitmask_t * bitmask;
+
+void setUp(void)
+{
+    bitmask = bitmask_init(BITMASK_BYTE_LENGTH);
 }
 
-void tearDown()
+void tearDown(void)
 {
+    bitmask_fini(bitmask);
+}
+
+void test_bitmask_init_should_initialize_bitmask(void)
+{
+    TEST_ASSERT_NOT_NULL(bitmask);
+    TEST_ASSERT_EQUAL_UINT(BITMASK_BYTE_LENGTH, bitmask->byte_length);
+
+    char expected_mask[BITMASK_BYTE_LENGTH] = "";
+    TEST_ASSERT_EQUAL_MEMORY(expected_mask, bitmask->mask, BITMASK_BYTE_LENGTH);
+}
+
+void test_bitmask_functions_throw_exception_on_out_of_bounds_indices(void)
+{
+    CEXCEPTION_T e_expected = EXCEPTION_OUT_OF_BOUNDS;
+    CEXCEPTION_T e_actual = EXCEPTION_NO_EXCEPTION;
+
+    TRY_FN_CATCH_EXCEPTION(bitmask_set_bit(bitmask, -1));
+    TRY_FN_CATCH_EXCEPTION(bitmask_set_bit(bitmask, 32));
+    TRY_FN_CATCH_EXCEPTION(bitmask_clear_bit(bitmask, -1));
+    TRY_FN_CATCH_EXCEPTION(bitmask_clear_bit(bitmask, 32));
+    TRY_FN_CATCH_EXCEPTION(bitmask_toggle_bit(bitmask, -1));
+    TRY_FN_CATCH_EXCEPTION(bitmask_toggle_bit(bitmask, 32));
+
+    e_expected = EXCEPTION_NO_EXCEPTION;
+    TRY_FN_CATCH_EXCEPTION(bitmask_set_bits(bitmask, 0, 0));
+    e_expected = EXCEPTION_OUT_OF_BOUNDS;
+    TRY_FN_CATCH_EXCEPTION(bitmask_set_bits(bitmask, 0, -1));
+    TRY_FN_CATCH_EXCEPTION(bitmask_set_bits(bitmask, -1, 0));
+    TRY_FN_CATCH_EXCEPTION(bitmask_set_bits(bitmask, 32, 0));
+    TRY_FN_CATCH_EXCEPTION(bitmask_set_bits(bitmask, 0, 32));
+
+    e_expected = EXCEPTION_NO_EXCEPTION;
+    TRY_FN_CATCH_EXCEPTION(bitmask_clear_bits(bitmask, 0, 0));
+    e_expected = EXCEPTION_OUT_OF_BOUNDS;
+    TRY_FN_CATCH_EXCEPTION(bitmask_clear_bits(bitmask, 0, -1));
+    TRY_FN_CATCH_EXCEPTION(bitmask_clear_bits(bitmask, -1, 0));
+    TRY_FN_CATCH_EXCEPTION(bitmask_clear_bits(bitmask, 32, 0));
+    TRY_FN_CATCH_EXCEPTION(bitmask_clear_bits(bitmask, 0, 32));
+
+    e_expected = EXCEPTION_NO_EXCEPTION;
+    TRY_FN_CATCH_EXCEPTION(bitmask_toggle_bits(bitmask, 0, 0));
+    e_expected = EXCEPTION_OUT_OF_BOUNDS;
+    TRY_FN_CATCH_EXCEPTION(bitmask_toggle_bits(bitmask, 0, -1));
+    TRY_FN_CATCH_EXCEPTION(bitmask_toggle_bits(bitmask, -1, 0));
+    TRY_FN_CATCH_EXCEPTION(bitmask_toggle_bits(bitmask, 32, 0));
+    TRY_FN_CATCH_EXCEPTION(bitmask_toggle_bits(bitmask, 0, 32));
+
+    TRY_FN_CATCH_EXCEPTION(bitmask_is_bit_set(bitmask, -1));
+    TRY_FN_CATCH_EXCEPTION(bitmask_is_bit_set(bitmask, 32));
+
+    e_expected = EXCEPTION_NO_EXCEPTION;
+    TRY_FN_CATCH_EXCEPTION(bitmask_are_bits_set(bitmask, 0, 0));
+    e_expected = EXCEPTION_OUT_OF_BOUNDS;
+    TRY_FN_CATCH_EXCEPTION(bitmask_are_bits_set(bitmask, 0, -1));
+    TRY_FN_CATCH_EXCEPTION(bitmask_are_bits_set(bitmask, -1, 0));
+    TRY_FN_CATCH_EXCEPTION(bitmask_are_bits_set(bitmask, 32, 0));
+    TRY_FN_CATCH_EXCEPTION(bitmask_are_bits_set(bitmask, 0, 32));
+}
+
+void test_bitmask_is_and_set_bit_should_set_and_check_the_nth_bit(void)
+{
+    TEST_ASSERT_EQUAL_INT(0, bitmask_is_bit_set(bitmask, 0));
+    bitmask_set_bit(bitmask, 0);
+    TEST_ASSERT_EQUAL_INT(1, bitmask_is_bit_set(bitmask, 0));
+    bitmask_set_bit(bitmask, 0);
+    TEST_ASSERT_EQUAL_INT(1, bitmask_is_bit_set(bitmask, 0));
     
+    TEST_ASSERT_EQUAL_INT(0, bitmask_is_bit_set(bitmask, 31));
+    bitmask_set_bit(bitmask, 31);
+    TEST_ASSERT_EQUAL_INT(1, bitmask_is_bit_set(bitmask, 31));
+    bitmask_set_bit(bitmask, 31);
+    TEST_ASSERT_EQUAL_INT(1, bitmask_is_bit_set(bitmask, 31));
 }
 
-void test_bitmask_init_should_initialize_bitmask()
+void test_bitmask_clear_bit_should_clear_the_nth_bit(void)
 {
-    TEST_IGNORE_MESSAGE("TODO: Implement the bitmask!");
+    bitmask_set_bit(bitmask, 0);
+    bitmask_clear_bit(bitmask, 0);
+    TEST_ASSERT_EQUAL_INT(0, bitmask_is_bit_set(bitmask, 0));
+    bitmask_clear_bit(bitmask, 0);
+    TEST_ASSERT_EQUAL_INT(0, bitmask_is_bit_set(bitmask, 0));
+
+    bitmask_set_bit(bitmask, 31);
+    bitmask_clear_bit(bitmask, 31);
+    TEST_ASSERT_EQUAL_INT(0, bitmask_is_bit_set(bitmask, 31));
+    bitmask_clear_bit(bitmask, 31);
+    TEST_ASSERT_EQUAL_INT(0, bitmask_is_bit_set(bitmask, 31));
+}
+
+void test_bitmask_toggle_bit_should_toggle_the_nth_bit(void)
+{
+    bitmask_set_bit(bitmask, 0);
+    bitmask_toggle_bit(bitmask, 0);
+    TEST_ASSERT_EQUAL_INT(0, bitmask_is_bit_set(bitmask, 0));
+    bitmask_toggle_bit(bitmask, 0);
+    TEST_ASSERT_EQUAL_INT(1, bitmask_is_bit_set(bitmask, 0));
+    bitmask_toggle_bit(bitmask, 0);
+    TEST_ASSERT_EQUAL_INT(0, bitmask_is_bit_set(bitmask, 0));
+
+    bitmask_toggle_bit(bitmask, 31);
+    TEST_ASSERT_EQUAL_INT(1, bitmask_is_bit_set(bitmask, 31));
+    bitmask_toggle_bit(bitmask, 31);
+    TEST_ASSERT_EQUAL_INT(0, bitmask_is_bit_set(bitmask, 31));
+    bitmask_toggle_bit(bitmask, 31);
+    TEST_ASSERT_EQUAL_INT(1, bitmask_is_bit_set(bitmask, 31));
 }
