635bf3342b3a134386396a613bea52aef80ba98d
Xunnamius (Lancelot) <Xunnamius@users.noreply.github.com>
2017-10-17 08:53:10 -0500

    incremental

--- test/test_buselfs.c
+++ test/test_buselfs.c
@@ -1643,9 +1643,10 @@ void test_blfs_energymon_collect_metrics_works_as_expected(void)
     sleep(3);
     blfs_energymon_collect_metrics(&metrics_end, buselfs_state);
 
-    TEST_ASSERT_GREATER_THAN_MESSAGE(metrics_start.energy_uj, metrics_end.energy_uj, "metrics_end.energy_uj <= metrics_start.energy_uj");
+    TEST_ASSERT_TRUE_MESSAGE(metrics_start.energy_uj, "metrics_start.energy_uj == 0");
+    TEST_ASSERT_NOT_EQUAL_MESSAGE(metrics_start.energy_uj, metrics_end.energy_uj, "metrics_end.energy_uj <= metrics_start.energy_uj");
 
-    TEST_ASSERT_GREATER_THAN_MESSAGE(0, metrics_start.time_ns, "metrics_start.time_ns <= 0");
+    TEST_ASSERT_TRUE_MESSAGE(metrics_start.time_ns, "metrics_start.time_ns == 0");
     TEST_ASSERT_NOT_EQUAL_MESSAGE(metrics_start.time_ns, metrics_end.time_ns, "metrics_end.time_ns == metrics_start.time_ns");
 }
 
